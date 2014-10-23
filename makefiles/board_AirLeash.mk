8377e2dc9276e005e306cda91e479c32320b0bf6
Alexey Luchko <luch@airdog.com>
2014-10-23 15:08:30 +0300

    [AirLeash] Copy px4fmu configuration.

---
+++ makefiles/board_AirLeash.mk
@@ -0,0 +1,7 @@
+#
+# Configure the toolchain
+#
+CONFIG_ARCH			 = CORTEXM4F
+CONFIG_BOARD			 = AIRLEASH
+
+include $(PX4_MK_DIR)/toolchain_gnu-arm-eabi.mk
---
+++ src/drivers/boards/AirLeash/module.mk
@@ -0,0 +1,4 @@
+SRCS = init.c \
+	led.c \
+	spi.c \
+	usb.c
