41f26e1540d6ab02800b09a8b9b5f3888e8a9fcb
Anthony Kenga <anton.k@airdog.com>
2014-10-25 21:22:08 +0300

    Fixes, comments and expanded distance calculation

--- src/modules/navigator/path_follow.cpp
+++ src/modules/navigator/path_follow.cpp
@@ -15,8 +15,9 @@ PathFollow::PathFollow(Navigator *navigator, const char *name):
 		NavigatorMode(navigator, name),
 		_last_trajectory_time(0),
 		_saved_trajectory(),
-		_trajectory_distance(0),
-		_has_valid_setpoint(false) {
+		_trajectory_distance(0.0f),
+		_has_valid_setpoint(false),
+		_desired_speed(0.0f) {
 
 }
 PathFollow::~PathFollow() {
@@ -28,16 +29,17 @@ bool PathFollow::init() {
 }
 void PathFollow::on_inactive() {
 	// TODO! Consider if we want to continue collecting trajectory data while inactive
-	// update_trajectory();
+	// update_saved_trajectory();
 }
 void PathFollow::on_activation() {
 	_has_valid_setpoint = false;
 	// TODO! Consider if we really want to reset the trajectory state
 	_saved_trajectory.do_empty();
-	update_trajectory();
+	update_saved_trajectory();
 	global_pos = _navigator->get_global_position();
 	pos_sp_triplet = _navigator->get_position_setpoint_triplet();
 	pos_sp_triplet->next.valid = false;
+
 	// Reset position setpoint to shoot and loiter until we get an acceptable trajectory point
 	pos_sp_triplet->current.type = SETPOINT_TYPE_POSITION;
 	pos_sp_triplet->current.lat = global_pos->lat;
@@ -47,9 +49,11 @@ void PathFollow::on_activation() {
 	_navigator->set_position_setpoint_triplet_updated();
 }
 void PathFollow::on_active() {
-	update_trajectory();
+	update_saved_trajectory();
 	pos_sp_triplet = _navigator->get_position_setpoint_triplet();
+
 	if (!_has_valid_setpoint) {
+		// Waiting for the first trajectory point
 		_has_valid_setpoint = _saved_trajectory.pop(_actual_point);
 		if (_has_valid_setpoint) {
 			update_setpoint(_actual_point);
@@ -58,6 +62,7 @@ void PathFollow::on_active() {
 	else {
 		// TODO! Update the check for moving points and check for Follow Path compatibility
 		if (check_current_pos_sp_reached()) {
+			// We've reached the actual setpoint
 			_trajectory_distance -= get_distance_to_next_waypoint(pos_sp_triplet->current.lat,
 					pos_sp_triplet->current.lon, _actual_point.lat, _actual_point.lon);
 			_has_valid_setpoint = _saved_trajectory.pop(_actual_point);
@@ -66,12 +71,24 @@ void PathFollow::on_active() {
 			}
 		}
 	}
+
+	// In any scenario point the camera to target
+	point_camera_to_target(&(pos_sp_triplet->current));
+
+	// If we have a setpoint, update speed
+	if (_has_valid_setpoint) {
+		_desired_speed = calculate_desired_speed(calculate_current_distance());
+		// TODO! Add setpoint modification
+	}
+
+	// In any scenario setpoint could have changed
+	_navigator->set_position_setpoint_triplet_updated();
 }
 void PathFollow::execute_vehicle_command() {
 
 }
 
-void PathFollow::update_trajectory() {
+void PathFollow::update_saved_trajectory() {
 	struct external_trajectory_s *target_trajectory = _navigator->get_target_trajectory();
 	// Assuming timestamp won't be 0 on first call
 	if (_last_trajectory_time != target_trajectory->timestamp && target_trajectory->point_type != 0) {
@@ -87,7 +104,7 @@ void PathFollow::update_trajectory() {
 		_last_point.lon = target_trajectory->lon;
 		_last_point.alt = target_trajectory->alt;
 		if (!_saved_trajectory.add(_last_point, true)) {
-			mavlink_log_info(_mavlink_fd, "Trajectory overflow!");
+			mavlink_log_critical(_mavlink_fd, "Trajectory overflow!");
 		}
 	}
 }
@@ -97,21 +114,18 @@ void PathFollow::update_setpoint(const buffer_point_s &desired_point) {
 	// TODO! Parameters
 	float offset = 5;
 	pos_sp_triplet = _navigator->get_position_setpoint_triplet();
-	// TODO! Is it needed?
+	// TODO! Is the previous point needed?
 	pos_sp_triplet->previous = pos_sp_triplet->current;
 	pos_sp_triplet->current.type = SETPOINT_TYPE_POSITION;
 	pos_sp_triplet->current.lat = desired_point.lat;
 	pos_sp_triplet->current.lon = desired_point.lon;
 	pos_sp_triplet->current.alt = desired_point.alt + offset;
-	point_camera_to_target(&(pos_sp_triplet->current));
-	_navigator->set_position_setpoint_triplet_updated();
 }
 
-float PathFollow::calculate_desired_speed(float distance, float target_speed) {
+float PathFollow::calculate_desired_speed(float distance) {
 	// TODO! Parameters both for min, max, ok and for functions
 	float max_dist = 20.0f, min_dist = 5.0f, ok_dist = 10.0f;
 	float max_speed = 11.1111f;
-	float res;
 	if (distance <= min_dist) {
 		return 0;
 	}
@@ -119,7 +133,11 @@ float PathFollow::calculate_desired_speed(float distance, float target_speed) {
 		// TODO! Max speed
 		return max_speed;
 	}
-	else if (distance >= ok_dist) {
+
+	float res;
+	target_pos = _navigator->get_target_position();
+	float target_speed = float(sqrt(double(target_pos->vel_n * target_pos->vel_n + target_pos->vel_e * target_pos->vel_e)));
+	if (distance >= ok_dist) {
 		// TODO! Simplify and add precalculated values
 		res = target_speed * ((distance - ok_dist)*(distance - ok_dist)*4.0f/(max_dist-ok_dist)/(max_dist-ok_dist)+1.0f);
 	}
@@ -133,5 +151,10 @@ float PathFollow::calculate_desired_speed(float distance, float target_speed) {
 }
 
 float PathFollow::calculate_current_distance() {
-
+	float res = _trajectory_distance;
+	global_pos = _navigator->get_global_position();
+	res += get_distance_to_next_waypoint(global_pos->lat, global_pos->lon, _actual_point.lat, _actual_point.lon);
+	target_pos = _navigator->get_target_position();
+	res += get_distance_to_next_waypoint(_last_point.lat, _last_point.lon, target_pos->lat, target_pos->lon);
+	return res;
 }
