3049a36acf67e232312d2416494376dbf2418b7e
Alexey Luchko <luch@airdog.com>
2014-10-30 12:31:33 +0200

    [AirDOG] Renames after copy.

--- src/drivers/boards/AirDogFMU/module.mk
+++ src/drivers/boards/AirDogFMU/module.mk
@@ -1,10 +1,5 @@
-#
-# Board-specific startup code for the PX4FMUv2
-#
-
-SRCS		 = px4fmu_can.c \
-		   px4fmu2_init.c \
-		   px4fmu_pwm_servo.c \
-		   px4fmu_spi.c \
-		   px4fmu_usb.c \
-		   px4fmu2_led.c
+SRCS = init.c \
+	led.c \
+	pwm_servo.c \
+	spi.c \
+	usb.c
