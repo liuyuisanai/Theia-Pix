1dd2e7a37f12df98305ca39371ed7647c6eccd9c
Alexey Luchko <luch@airdog.com>
2015-01-05 18:31:31 +0300

    [AirLeash] indication: blinks for loiter mode.

--- src/modules/indication/status_leash.hpp
+++ src/modules/indication/status_leash.hpp
@@ -25,6 +25,7 @@ static param_t param_use_blue_led;
 static int32_t use_blue_led;
 
 static bool pos_valid, link_valid, force_update;
+static bool loiter_mode;
 
 void
 init()
@@ -35,6 +36,7 @@ init()
 	force_update = true;
 	pos_valid = false;
 	link_valid = false;
+	loiter_mode = false;
 	use_blue_led = -1;
 }
 
@@ -55,27 +57,39 @@ update(hrt_abstime now)
 	bool changed_link_valid = link_valid != link_ok;
 	link_valid = link_ok;
 
+	bool now_loiter = link_valid
+			and (airdog_status.state_main == MAIN_STATE_LOITER);
+	bool changed_mode = loiter_mode != now_loiter;
+	loiter_mode = now_loiter;
+	if (changed_mode)
+		fprintf(stderr, "State main %i %i\n", airdog_status.state_main, loiter_mode);
+
 	int32_t x;
 	param_get(param_use_blue_led, &x);
 	bool changed_use_blue_led = x != use_blue_led;
 	use_blue_led = x;
 
 
-	if (force_update or changed_link_valid or changed_pos_valid or changed_use_blue_led)
-	{
+	if (force_update or changed_use_blue_led
+	or changed_link_valid or changed_pos_valid or changed_mode
+	) {
 		uint32_t pattern;
 
 		if (use_blue_led) {
 			pattern = pos_valid ? 0xFFFFFFFe : 0x80000000;
 			leds::set_pattern_repeat(LED_STATUS, pattern);
 
-			pattern = link_valid ? 0xFFFFFFFd : 0x80000000;
+			if (link_valid)
+				pattern = loiter_mode ? 0xFF00FF00 : 0xFFFFFFFd;
+			else
+				pattern = 0x80000000;
+
 			leds::set_pattern_repeat(LED_LEASHED, pattern);
 		}
 		else
 		{
 			if (pos_valid and link_valid)
-				pattern = 0xFFFFFFFe;
+				pattern = loiter_mode ? 0xFF00FF00 : 0xFFFFFFFe;
 			else if (pos_valid or link_valid)
 				pattern = 0x88000000;
 			else
