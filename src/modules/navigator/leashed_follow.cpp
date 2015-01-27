fe2a19cfdc23aec3182063d0fd8cbb4e0a1534d9
Max Shvetsov <maxim.shvetsov@airdog.com>
2015-01-27 18:51:57 +0200

    [cable park] added RTL, Land commands

--- src/modules/navigator/leashed_follow.cpp
+++ src/modules/navigator/leashed_follow.cpp
@@ -103,10 +103,31 @@ Leashed::execute_vehicle_command() {
     // Update _parameter values with the latest navigator_mode parameters
     memcpy(&_parameters, &(NavigatorMode::_parameters), sizeof(_parameters));
 	vehicle_command_s cmd = _vcommand;
+	if (cmd.command == VEHICLE_CMD_DO_SET_MODE){
+
+		//uint8_t base_mode = (uint8_t)cmd.param1;
+		uint8_t main_mode = (uint8_t)cmd.param2;
+
+		if (main_mode == PX4_CUSTOM_MAIN_MODE_RTL) {
+
+			commander_request_s *commander_request = _navigator->get_commander_request();
+			commander_request->request_type = V_MAIN_STATE_CHANGE;
+			commander_request->main_state = MAIN_STATE_RTL;
+			_navigator->set_commander_request_updated();
+
+		}
+	}
 	if (cmd.command == VEHICLE_CMD_NAV_REMOTE_CMD) {
 		REMOTE_CMD remote_cmd = (REMOTE_CMD)cmd.param1;
 		math::Vector<2> offset =_afollow_offset;
 		switch(remote_cmd){
+			case  REMOTE_CMD_LAND_DISARM: {
+                commander_request_s *commander_request = _navigator->get_commander_request();
+                commander_request->request_type = V_MAIN_STATE_CHANGE;
+                commander_request->main_state = MAIN_STATE_EMERGENCY_LAND;
+                _navigator->set_commander_request_updated();
+                break;
+            }
 			case REMOTE_CMD_PLAY_PAUSE: {
 				commander_request_s *commander_request = _navigator->get_commander_request();
 				commander_request->request_type = V_MAIN_STATE_CHANGE;
