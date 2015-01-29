1970c30e21ef24e9a52c6ea9f23e7d83426907c4
Anthony Kenga <anton.k@airdog.com>
2015-01-29 13:40:58 +0200

    Changed gyro calibration defaults Prevents potential 16 minute freezes if the sensor is not responding

--- src/modules/airdog/calibrator/calibrator.hpp
+++ src/modules/airdog/calibrator/calibrator.hpp
@@ -7,13 +7,13 @@ namespace calibration {
 /* Starts gyroscope calibration procedure.
  * mavlink_fd - if not zero, then messages will be sent via mavlink too
  * sample_count - number of samples to average when calibrating offsets. Default: 5000. Make it positive or I will shoot you.
- * max_error_count - number of errors tolerated. Polling will return error if error count gets lager than this parameter. Default: 1000.
- * timeout - timeout for each poll request in ms. Worst case process will hang for timeout*(max_error_count + 1) ms. So be considerate. Default: 1000.
+ * max_error_count - number of errors tolerated. Polling will return error if error count gets lager than this parameter. Default: 100.
+ * timeout - timeout for each poll request in ms. Worst case process will hang for timeout*(max_error_count + 1) ms. So be considerate. Default: 200.
  * @return true if calibration was successful, false otherwise
  */
 __EXPORT bool calibrate_gyroscope(int mavlink_fd=0, const unsigned int sample_count=5000,
-						const unsigned int max_error_count=1000,
-						const int timeout=1000);
+						const unsigned int max_error_count=100,
+						const int timeout=200);
 
 /* Starts magnetometer calibration procedure.
  * mavlink_fd - if not zero, then messages will be sent via mavlink too
