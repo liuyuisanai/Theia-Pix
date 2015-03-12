3635b6d58429b7ef3eb53d29d7a617e28317152b
Alexey Luchko <luch@airdog.com>
2015-03-12 12:07:42 +0200

    [BT740 #89925824] some debug improvements.

--- src/drivers/bluetooth21/debug.hpp
+++ src/drivers/bluetooth21/debug.hpp
@@ -6,21 +6,26 @@
 #include <syslog.h>
 #endif
 
-#define D_ASSERT ASSERT
-//#ifdef CONFIG_DEBUG_BLUETOOTH21
-//#define D_ASSERT(...) ASSERT
-//#else
-//#define D_ASSERT
-//#endif
+#ifdef CONFIG_DEBUG_BLUETOOTH21
+#define D_ASSERT(...) ASSERT( __VA_ARGS__ )
+#else
+#define D_ASSERT(...)
+#endif
 
 namespace BT
 {
 
 template <typename ... types>
 inline void
-dbg(const char fmt[], types ... args)
+dbg(const char fmt[], const types & ... args)
 {
 #ifdef CONFIG_DEBUG_BLUETOOTH21
+#ifdef MODULE_COMMAND
+# define MODULE_COMMAND_XSTR(X) #X
+# define MODULE_COMMAND_STR(X) MODULE_COMMAND_XSTR(X)
+	// syslog does not append newlines itself.
+	syslog(MODULE_COMMAND_STR(MODULE_COMMAND) ": ");
+#endif
 	syslog(fmt, args...);
 #endif
 }
