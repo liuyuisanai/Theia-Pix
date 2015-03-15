bfadded61fc95f377943123f9a8fd663b3c54677
Alexey Luchko <luch@airdog.com>
2015-03-15 20:32:48 +0200

    [BT740 #90206196] io_tty make verbose.

--- src/drivers/bluetooth21/io_tty.hpp
+++ src/drivers/bluetooth21/io_tty.hpp
@@ -3,6 +3,10 @@
 #include <fcntl.h>
 #include <termios.h>
 
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+
 namespace BT
 {
 
@@ -10,14 +14,25 @@ inline bool
 tty_set_speed(int fd, speed_t speed)
 {
 	struct termios mode;
-	return tcgetattr(fd, &mode) == 0
+	bool ok = tcgetattr(fd, &mode) == 0
 		and cfsetspeed(&mode, speed) == 0
 		and tcsetattr(fd, TCSANOW, &mode) == 0;
+	if (not ok)
+		perror("tty_set_speed");
+	return ok;
 }
 
 inline int
 tty_open(const char name[])
-{ return open(name, O_RDWR | O_NONBLOCK | O_NOCTTY); }
+{
+       int r = open(name, O_RDWR | O_NONBLOCK | O_NOCTTY);
+       if (r < 0)
+       {
+		fprintf(stderr, "tty_open(\"%s\"): %s\n",
+				name, strerror(errno));
+       }
+       return r;
+}
 
 }
 // end of namespace BT
