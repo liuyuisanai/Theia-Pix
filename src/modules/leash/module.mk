dba8d6391484113dc2a26eea6e0be41f97a52a75
Alexey Luchko <luch@airdog.com>
2015-06-15 21:11:01 +0300

    [AirLeash] hack to reduce kbd code size.

--- src/modules/leash/module.mk
+++ src/modules/leash/module.mk
@@ -1,7 +1,8 @@
 MODULE_COMMAND = $(notdir $(shell pwd))
 
 SRCS = main.cpp tones.cpp uorb_functions.cpp
-EXTRACXXFLAGS = -std=gnu++11 -Dmain=$(MODULE_COMMAND)_main
+EXTRACXXFLAGS = -std=gnu++11 -Dmain=$(MODULE_COMMAND)_main \
+	-finstrument-functions-exclude-file-list=kbd_h,/revision/
 
 #
 # All error messages are essential for finding conflicting template candidates.
