a43795253396e7e8229c1ecf623de153901bdfcc
Alexey Luchko <luch@airdog.com>
2015-07-08 17:15:09 +0300

    [mobile] StatusOverall: battery level.

--- src/modules/mobile/status.hpp
+++ src/modules/mobile/status.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/vehicle_status.h>
 
 #include "protocol.h"
 
@@ -9,14 +10,29 @@
 struct StatusOverall
 {
 	Subscription<vehicle_global_position_s, ORB_ID(vehicle_global_position)> gpos;
+	Subscription<vehicle_status_s, ORB_ID(vehicle_status)> status;
 };
 
-StatusOverallReply
-reply(StatusOverall & self)
+static inline void
+battery(StatusOverall & self, StatusOverallReply & r)
 {
-	StatusOverallReply r;
+	auto status = orb_read(self.status);
+	r.battery_level = status.battery_remaining * 100.0f;
+}
+
+static inline void
+position(StatusOverall & self, StatusOverallReply & r)
+{
 	auto gpos = orb_read(self.gpos);
 	r.gps_error_horizontal = gpos.eph;
 	r.gps_error_vertical = gpos.epv;
+}
+
+StatusOverallReply &&
+reply(StatusOverall & self)
+{
+	StatusOverallReply r;
+	battery(self, r);
+	position(self, r);
 	return std::move(r);
 }
