8d2fa8d6df2118041741b2453f31e481df10b794
Alexey Luchko <luch@airdog.com>
2015-08-05 20:32:48 +0300

    [DOG_PRINT] save errno, allow debug-only variables.

--- src/include/dog_debug.hpp
+++ src/include/dog_debug.hpp
@@ -2,16 +2,35 @@
 
 #ifdef ENABLE_DOG_DEBUG
 
-# ifdef __cplusplus
-#  include <cstdio>
-#  define DOG_PRINT(...) std::fprintf(stderr, __VA_ARGS__)
-# else
-#  include <stdio.h>
-#  define DOG_PRINT(...) fprintf(stderr, __VA_ARGS__)
-# endif
+#ifdef __cplusplus
+# include <cstdio>
+#else
+# include <stdio.h>
+#endif
+
+#define DOG_PRINT(...) do {                              \
+		const int save_errno = errno;            \
+		fprintf(stderr, __VA_ARGS__);            \
+		fflush(stderr);                          \
+		errno = save_errno;			 \
+	} while (0)
 
 #else
 
-#define DOG_PRINT(...) {}
+/*
+ * Prevent unused-variable errors and warnings while debug is disabled.
+ *
+ * With gcc 4.7 empty function is more effective than
+ *
+ *        if(ENABLE_DOG_DEBUG) {...}
+ *
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-prototypes"
+
+static inline void
+DOG_PRINT(const char fmt[], ...) {}
+
+#pragma GCC diagnostic pop
 
 #endif
